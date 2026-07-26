010e533a56e97cda848275136155ea6cd3267bf2
karlosolliveira <karlosolliveira@gmail.com>
2026-07-26 21:42:45 +0100

    Back to debug build - fix notification access via ADB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,22 +13,9 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    signingConfigs {
-        create("release") {
-            storeFile = file(System.getProperty("user.home") + "/.android/debug.keystore")
-            storePassword = "android"
-            keyAlias = "androiddebugkey"
-            keyPassword = "android"
-        }
-    }
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
-            isDebuggable = false
-        }
-        debug {
-            isDebuggable = true
         }
     }
     compileOptions {
